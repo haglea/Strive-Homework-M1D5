25d61297a2787e55d861b5c55e8d32d89759340d
haglea <haglea@icloud.com>
2021-05-21 16:10:23 +0200

    Exercise 5 solved
    method String.startsWith()
    Condition

--- D5.js
+++ D5.js
@@ -109,7 +109,25 @@ Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
+console.log("\n--------------EXERCISE 5--------------")
+
+let strivify = function (aString) {
+
+    let addWordStrive = "Strive" + aString
+
+    if (aString.startsWith("Strive")) {
+
+        console.log(aString)
+
+    } else {
+
+    console.log(addWordStrive)
+
+    }
+}
+
+strivify("school")
+strivify("Striveschool")
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
