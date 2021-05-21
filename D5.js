ae385074ed27e27124bd58f39057b7331bf9d749
haglea <haglea@icloud.com>
2021-05-21 14:43:13 +0200

    Solved Exercise 1
    Solved Exercise 1

--- D5.js
+++ D5.js
@@ -10,6 +10,16 @@ ASSIGNMENT RULES
 Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
 */
 
+console.log("\n--------------EXERCISE 1--------------")
+
+let area = function (l1, l2) {
+
+    let areaOfRectangle = l1 * l2
+    console.log(areaOfRectangle)
+
+}
+area(3, 2)
+
 /* EXERCISE 2
 Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
